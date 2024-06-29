aadb745b87ecfc424d759f0b2d8d5b75349aa9f6
IbrahimRiani <104872719+IbrahimRiani@users.noreply.github.com>
2024-06-29 19:49:31 +0200

    Resolved merge conflicts 2

--- src/app/pages/tea-detail/tea-detail.component.ts
+++ src/app/pages/tea-detail/tea-detail.component.ts
@@ -1,8 +1,5 @@
 import { Component } from '@angular/core';
-<<<<<<< HEAD
 import { Router } from '@angular/router';
-=======
->>>>>>> 99d967d01132b012c1cc9f99b4c834d1a3da5bbd
 
 @Component({
   selector: 'app-tea-detail',
@@ -10,13 +7,9 @@ import { Router } from '@angular/router';
   styleUrls: ['./tea-detail.component.scss']
 })
 export class TeaDetailComponent {
-<<<<<<< HEAD
   constructor(private router: Router) {}
 
   navigateToTeaList(): void {
     this.router.navigate(['/tea-list']);
   }
-=======
-
->>>>>>> 99d967d01132b012c1cc9f99b4c834d1a3da5bbd
 }
--- src/app/pages/tea-list/tea-list.component.ts
+++ src/app/pages/tea-list/tea-list.component.ts
@@ -1,8 +1,5 @@
 import { Component } from '@angular/core';
-<<<<<<< HEAD
 import { Router } from '@angular/router';
-=======
->>>>>>> 99d967d01132b012c1cc9f99b4c834d1a3da5bbd
 
 @Component({
   selector: 'app-tea-list',
@@ -10,13 +7,9 @@ import { Router } from '@angular/router';
   styleUrls: ['./tea-list.component.scss']
 })
 export class TeaListComponent {
-<<<<<<< HEAD
   constructor(private router: Router) {}
 
   goToDetail(id: number) {
     this.router.navigate(['/tea-detail', id]);
   }
-=======
-
->>>>>>> 99d967d01132b012c1cc9f99b4c834d1a3da5bbd
 }
